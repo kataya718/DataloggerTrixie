d4389a78045b0f7216976ce146c87029fc67ef90
kataya <katayalive@gmail.com>
2026-03-10 11:48:28 +0100

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,4 +1,11 @@
 <?php
+include "header.php";
+
+/* DB CONNECT — pas aan naar jouw setup */
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 // Settings
 // host, user and password settings
 $host = "localhost";
@@ -6,7 +13,7 @@ $user = "logger";
 $password = "paswoord";
 $database = "temperatures";
 
-//Tot hoeveel uur terug wil je de data tonen wanneer dit php opgevraagd wordt vanuit browser
+//Tot hoeveel uur terug wil je de data tonen wanneer dit php opgevraagd wordt va                                                                                                                                                             nuit browser
 $hours = 24;
 
 // make connection to database
@@ -18,9 +25,9 @@ mysqli_select_db($connectdb,$database)
 or die ("Cannot select database");
 
 // sql command that selects all entires from current time and X hours backwards
-$sql="SELECT * FROM temperaturedata WHERE dateandtime >= (NOW() - INTERVAL $hours HOUR) order by dateandtime desc";
+$sql="SELECT * FROM temperaturedata WHERE dateandtime >= (NOW() - INTERVAL $hour                                                                                                                                                             s HOUR) order by dateandtime desc";
 
-//NOTE: If you want to show all entries from current date in web page uncomment line below by removing //
+//NOTE: If you want to show all entries from current date in web page uncomment                                                                                                                                                              line below by removing //
 //$sql="select * from temperaturedata where date(dateandtime) = curdate();";
 
 // set query to variable
@@ -37,17 +44,17 @@ $temperatures = mysqli_query($connectdb,$sql);
 
   <h3>Temperatuur en vochtigheid - data </h3>
     <br>
-        <table width="800" border="1" cellpadding="1" cellspacing="1" align="center">
+        <table width="800" border="1" cellpadding="1" cellspacing="1" align="cen                                                                                                                                                             ter">
                         <tr>
-                        <th>Date</th>
+                        <th>Datum</th>
                         <th>Sensor</th>
-                        <th>Temperature</th>
-                        <th>Humidity</th>
+                        <th>Temperatuur</th>
+                        <th>Vochtigheid</th>
                         <tr>
 
                         <?php
-                                // loop all the results that were read from database and "draw" to web page
-                                while($temperature=mysqli_fetch_assoc($temperatures)){
+                                // loop all the results that were read from data                                                                                                                                                             base and "draw" to web page
+                                while($temperature=mysqli_fetch_assoc($temperatu                                                                                                                                                             res)){
                                 echo "<tr>";
                                 echo "<td>".$temperature['dateandtime']."</td>";
                                 echo "<td>".$temperature['sensor']."</td>";
