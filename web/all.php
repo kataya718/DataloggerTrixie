42e3c1616011105796126dbe5c8040281c4ba20c
kataya <katayalive@gmail.com>
2026-03-10 11:51:17 +0100

    Create all.php

---
+++ web/all.php
@@ -0,0 +1,11 @@
+<?php include "header.php"; ?>
+<h1>temperatuur & vochtigheid</h1>
+
+
+
+<canvas id="climateChart" height="120"></canvas>
+
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<script src="climate.js"></script>
+
+<?php include "footer.php"; ?>
