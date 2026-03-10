dcb366cd0dc828e639f1313e3ff5a2370a8d19df
kataya <katayalive@gmail.com>
2026-03-10 11:50:31 +0100

    Create maand.php

---
+++ web/maand.php
@@ -0,0 +1,19 @@
+<?php include "header.php"; ?>
+
+<h1>maand</h1>
+
+<div class="image-row">
+
+<div class="image-block">
+    <h2>temperatuur</h2>
+    <img src="MaandTemperatuur.png" alt="Maand temperatuur">
+</div>
+
+<div class="image-block">
+    <h2>vochtigheid</h2>
+    <img src="MaandVochtigheid.png" alt="Maand vochtigheid">
+</div>
+
+</div>
+
+<?php include "footer.php"; ?>
