f7621f2c619dbcacccc6e8a904cfd2f92ae0d3ba
kataya <katayalive@gmail.com>
2026-03-10 11:49:56 +0100

    Create week.php

---
+++ web/week.php
@@ -0,0 +1,19 @@
+<?php include "header.php"; ?>
+
+<h1>week</h1>
+
+<div class="image-row">
+
+<div class="image-block">
+    <h2>temperatuur</h2>
+    <img src="WeekVochtigheid.png" alt="Week temperatuur">
+</div>
+
+<div class="image-block">
+    <h2>vochtigheid</h2>
+    <img src="WeekVochtigheid.png" alt="Week vochtigheid">
+</div>
+
+</div>
+
+<?php include "footer.php"; ?>
