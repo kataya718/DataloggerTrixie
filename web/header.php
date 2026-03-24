b0ab73544f2895c5dc37d5e17e618e5bd8f9f7fe
kataya <katayalive@gmail.com>
2026-03-24 10:47:53 +0100

    Create header.php

---
+++ web/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>pi18</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <span class="logo">temp</span>
+    <nav>
+        <a href="index.php">tabel</a>
+        <a href="dag.php">dag</a>
+        <a href="week.php">week</a>
+        <a href="maand.php">maand</a>
+        <a href="all.php">all</a>
+    </nav>
+</header>
+
+<main>
